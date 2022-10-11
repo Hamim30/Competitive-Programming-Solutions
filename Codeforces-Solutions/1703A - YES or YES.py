70b5dfeb31866406485bc162980e827e7048cf12
Hamim z <67522425+Hamim30@users.noreply.github.com>
2022-10-11 18:18:58 +0600

    1703A - YES or YES?

---
+++ Codeforces-Solutions/1703A - YES or YES.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+for i in range (int(input())):
+    a=input()
+    if a in ["Yes","YEs","YES","yES","yEs","yes","yeS","YeS"]:
+        print("YES")
+    else:
+        print("NO")
+
